878f7a3f890ea73ddb34a6f6e54f15e4adf7cdc1
Jahongir Umirov <jahongirumirov@Jahongirs-MacBook-Pro.local>
2022-06-30 20:47:58 +0500

    blog_1

--- blog/settings.py
+++ blog/settings.py
@@ -19,7 +19,7 @@ from dotenv import load_dotenv
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog.settings.local")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
