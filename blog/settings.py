a1153c1f24859c83023e28636a666bf7957d5695
Jahongir Umirov <jahongirumirov@Jahongirs-MacBook-Pro.local>
2022-07-01 10:23:06 +0500

    blog_8

--- blog/settings.py
+++ blog/settings.py
@@ -16,6 +16,8 @@ import dj_database_url
 import os
 from dotenv import load_dotenv 
 
+
+
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
